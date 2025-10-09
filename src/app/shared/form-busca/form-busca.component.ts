b5652b9bd3305c825175bb916bcad81dd5ff42a6
Victoria Rodovalho <victorialicia22@gmail.com>
2025-10-09 19:14:02 -0300

    finalizando layout do form de busca

--- src/app/shared/form-busca/form-busca.component.ts
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -3,11 +3,18 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { CardComponent } from "../card/card.component";
 import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+
 
 @Component({
   selector: 'app-form-busca',
   standalone: true,
-  imports: [MatButtonToggleModule, CardComponent, MatIconModule, MatChipsModule],
+  imports: [MatButtonToggleModule, CardComponent, MatIconModule, MatChipsModule, MatFormFieldModule, MatIconModule, MatInputModule, MatButtonModule, MatDatepickerModule],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './form-busca.component.html',
   styleUrl: './form-busca.component.scss'
 })
