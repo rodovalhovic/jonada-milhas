8bc1d4b406b19bbc8df28ec30772ab3c68fb7fd8
Victoria Rodovalho <victorialicia22@gmail.com>
2025-10-09 22:28:14 -0300

    chamando dados de promoções do back

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BannerComponent } from "../../shared/banner/banner.component";
 import { ContainerComponent } from "../../shared/container/container.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
 import { CardBuscaComponent } from "../../shared/card-busca/card-busca.component";
 import { CardDepoimentosComponent } from "../../shared/card-depoimentos/card-depoimentos.component";
 import { FormBuscaComponent } from "../../shared/form-busca/form-busca.component";
+import { PromocaoService } from '../../core/services/promocao.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent, ContainerComponent, FooterComponent, CardBuscaComponent, CardDepoimentosComponent, FormBuscaComponent],
+  imports: [BannerComponent, ContainerComponent, FooterComponent, CardBuscaComponent, CardDepoimentosComponent, FormBuscaComponent,],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  constructor (){}
 
+  ngOnInit(): void {}
 }
--- src/app/shared/card-busca/card-busca.component.ts
+++ src/app/shared/card-busca/card-busca.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { PromocaoService } from '../../core/services/promocao.service';
+import { Promocao } from '../../core/types/types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-busca',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './card-busca.component.html',
   styleUrl: './card-busca.component.scss'
 })
-export class CardBuscaComponent {
+export class CardBuscaComponent implements OnInit {
+  promocoes: Promocao[] = [];
 
+  constructor(private servicoPromocao: PromocaoService) { }
+
+  ngOnInit(){
+    this.servicoPromocao.listar().subscribe(dados => this.promocoes = dados);
+  }
 }
